e9045c5aa30c89135727ad27f1a550bc0a320507
Ashique <ashique12009@gmail.com>
2026-02-15 15:17:25 +0200

    login form style

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,22 @@ function App() {
   return (
     <>
       <div className="main-container">
-        <h1>Quiz App</h1>
+        <div className="app-container">
+          <h1 className="text-center">Quiz App - Login</h1>
+          <form className="login-form">
+            <div className="form-group">
+              <label htmlFor="username">Username:</label>
+              <input type="text" id="username" name="username" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" name="password" required />
+            </div>
+            <button type="submit" className="login-button">
+              Login
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
