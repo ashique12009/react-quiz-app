a44abb6a0c7ec9f69c79bb8f60e868c2d079455a
Ashique <ashique12009@gmail.com>
2026-02-15 15:10:46 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Quiz App
+
+A simple quiz application with user authentication.
+
+## Database
+
+JSON-Server is the database here.
+
+## Few Screenshots:
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="main-container">
+        <h1>Quiz App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
